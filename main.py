bed0af6f7f6c9be1c3b45c045195326f46779994
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-03-07 22:51:02 -0600

    Tomando la IP del Usuario y usando redirecciones

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request,make_response,redirect # Importar Flask para poder trabajar con el
 
-app = Flask(__name__)
+app = Flask(__name__) #Crear la app
 
-@app.route('/')
+@app.route('/') #Entrada a la app
+def index():
+    user_ip = request.remote_addr #Obtener la IP del Usuario
+    response = make_response(redirect('/hello')) # Respuesta y redirección a Hello
+    response.set_cookie('user_ip',user_ip) # Mandamos un cookie con la IP
+    return response
+
+
+@app.route('/hello') #Primera ruta (Home)
 def hello():
-    return 'Hello World Flask!!'
+    user_ip = request.cookies.get('user_ip') #Leemos la cookie y obtenemos la IP
+    return 'Tu IP es {}'.format(user_ip) # Responder al usuario con su IP
+
