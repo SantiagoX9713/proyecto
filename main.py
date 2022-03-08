c7c7932c2ab288542dd9501bcf39ac3509f98e24
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-03-07 23:59:44 -0600

    Enviando contexto de una manera

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,make_response,redirect,render_template # Importar Flask para poder trabajar con el
 
 app = Flask(__name__) #Crear la app
-
+todos = ['TODO','TODO 1','TODO 2']
 @app.route('/') #Entrada a la app
 def index():
     user_ip = request.remote_addr #Obtener la IP del Usuario
@@ -13,5 +13,9 @@ def index():
 @app.route('/hello') #Primera ruta (Home)
 def hello():
     user_ip = request.cookies.get('user_ip') #Leemos la cookie y obtenemos la IP
-    return render_template('hello.html',user_ip=user_ip) # Responder al usuario con su IP en un template HTML
+    context = {
+        "user_ip":user_ip,
+        "todos":todos
+    } # Pasaremos contexto con un diccionario
+    return render_template('hello.html',context=context) # Responder al usuario con su IP en un template HTML
 
