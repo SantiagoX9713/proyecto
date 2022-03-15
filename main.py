63ffd8bc1ef81f6ada73407d12834347eff7b9c6
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-03-14 22:46:12 -0600

    Bootstrap y WTForms para renderear Formas

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from ensurepip import bootstrap
-from flask import Flask, request,make_response,redirect,render_template # Importar Flask para poder trabajar con el
+from flask import Flask, request,make_response,redirect,render_template,session # Importar Flask para poder trabajar con el
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__) #Crear la app
 bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
 todos = ['TODO','TODO 1','TODO 2']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -19,16 +30,18 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr #Obtener la IP del Usuario
     response = make_response(redirect('/hello')) # Respuesta y redirección a Hello
-    response.set_cookie('user_ip',user_ip) # Mandamos un cookie con la IP
+    session['user_ip'] = user_ip # Mandamos un session con la IP
     return response
 
 
 @app.route('/hello') #Primera ruta (Home)
 def hello():
-    user_ip = request.cookies.get('user_ip') #Leemos la cookie y obtenemos la IP
+    user_ip = session.get('user_ip') #Leemos la session y obtenemos la IP
+    login_form = LoginForm()
     context = {
-        "user_ip":user_ip,
-        "todos":todos
+        'user_ip':user_ip,
+        'todos':todos,
+        'login_form': login_form
     } # Pasaremos contexto con varibles en vez de un diccionario, usamos ** para hacer de cada llave/valor una variable :D
     return render_template('hello.html',**context) # Responder al usuario con su IP en un template HTML
 
