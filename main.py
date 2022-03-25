45d72aadd14ba7a626b09663b57eb8e035096f8c
Santiago Ramos Morales <santiagoramos198@gmail.com>
2022-03-25 00:20:25 -0600

    App Factory

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-from crypt import methods
 from ensurepip import bootstrap
-from flask import Flask, flash, request,make_response,redirect,render_template,session,url_for # Importar Flask para poder trabajar con el
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import flash, request,make_response,redirect,render_template,session,url_for # Importar Flask para poder trabajar con el
 import unittest
 from app import create_app
+from app.forms import LoginForm
+
 
 app = create_app() #Crear la app
 
 todos = ['TODO','TODO 1','TODO 2']
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de Usuario',validators=[DataRequired()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    submit = SubmitField('Enviar')
-
 
 @app.cli.command()
 def test():
